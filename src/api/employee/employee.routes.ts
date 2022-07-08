dadf1791c482b9ef7c5b6ba564f85fea91d6fc18
chrisflowmotion <chris@flow-motion.io>
2022-07-08 15:58:04 +0100

    fix comment

--- src/api/employee/employee.routes.ts
+++ src/api/employee/employee.routes.ts
@@ -5,8 +5,6 @@ import { ACCESS, authorize } from '../middlewares/auth.middleware';
 const router = Router();
 
 router.route('/all').get(authorize([ACCESS.EMPLOYEES_ALL]), Controller.getAllEmployees);
-
-// C.R.U.D single Employee
 router.route('/new').post(authorize([ACCESS.EMPLOYEES_NEW]), Controller.newEmployee);
 router.route('/get').get(authorize([ACCESS.EMPLOYEES_GET]), Controller.getEmployee);
 router.route('/update').put(authorize([ACCESS.EMPLOYEES_UPDATE]), Controller.updateEmployee);
