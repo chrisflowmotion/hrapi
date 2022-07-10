c9714d3e31b595a36da859477d04d33eee48bdc2
Arb-aya <chayden50@gmail.com>
2022-07-10 18:59:47 +0100

    Update privileges

--- scripts/create_user.sql
+++ scripts/create_user.sql
@@ -4,5 +4,8 @@ GRANT ALL PRIVILEGES ON hr_app.hremployee TO 'hr_app_api'@'localhost';
 GRANT ALL PRIVILEGES ON hr_app.hremergencycontacts TO 'hr_app_api'@'localhost';
 GRANT ALL PRIVILEGES ON hr_app.hrholidayrequests TO 'hr_app_api'@'localhost';
 GRANT ALL PRIVILEGES ON hr_app.hrsickdays TO 'hr_app_api'@'localhost';
+GRANT ALL PRIVILEGES ON hr_app.users TO 'hr_app_api'@'localhost';
+GRANT ALL PRIVILEGES ON hr_app.privileges TO 'hr_app_api'@'localhost';
+GRANT ALL PRIVILEGES ON hr_app.userprivileges TO 'hr_app_api'@'localhost';
 
 flush privileges;
