c21e02c666a12dd4e3ab83cb9f6be593082632f0
chrisflowmotion <chris@flow-motion.io>
2022-07-11 12:50:20 +0100

    Remove password from script

--- scripts/create_user.sql
+++ scripts/create_user.sql
@@ -1,4 +1,4 @@
-CREATE USER 'hr_app_api'@'localhost' IDENTIFIED BY 'VIy38ivOk1';
+CREATE USER 'hr_app_api'@'localhost' IDENTIFIED BY '<PASSWORD>';
 
 GRANT ALL PRIVILEGES ON hr_app.hremployee TO 'hr_app_api'@'localhost';
 GRANT ALL PRIVILEGES ON hr_app.hremergencycontacts TO 'hr_app_api'@'localhost';
